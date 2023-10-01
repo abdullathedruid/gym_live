3b76716c4cda088fee85be2e3436579085c961ac
abdullathedruid <abdulla.alkamil@gmail.com>
2023-10-01 23:41:44 +0100

    feat: add workout timer

--- lib/gym_live_web/live/workout.ex
+++ lib/gym_live_web/live/workout.ex
@@ -3,6 +3,17 @@ defmodule GymLiveWeb.GymLive.Workout do
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    if connected?(socket), do: :timer.send_interval(1000, self(), :tick)
+
+    socket
+    |> assign(seconds: 0)
+    |> then(&{:ok, &1})
+  end
+
+  @impl true
+  def handle_info(:tick, socket) do
+    socket
+    |> assign(seconds: socket.assigns.seconds + 1)
+    |> then(&{:noreply, &1})
   end
 end
