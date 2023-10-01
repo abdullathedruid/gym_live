bc2b8bffeaecc557d68a197b220a8faee988b778
abdullathedruid <abdulla.alkamil@gmail.com>
2023-10-01 22:05:10 +0100

    navigate to new workout

--- lib/gym_live_web/controllers/page_controller.ex
+++ lib/gym_live_web/controllers/page_controller.ex
@@ -2,8 +2,6 @@ defmodule GymLiveWeb.PageController do
   use GymLiveWeb, :controller
 
   def home(conn, _params) do
-    # The home page is often custom made,
-    # so skip the default app layout.
-    render(conn, :home, layout: false)
+    render(conn, :home)
   end
 end
