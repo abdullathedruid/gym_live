ecb7697cffc3ce2df04c7f68c38a12ab68164c3f
abdullathedruid <abdulla.alkamil@gmail.com>
2023-10-06 00:48:30 +0100

    add start workout button

--- lib/gym_live_web/live/workout.ex
+++ lib/gym_live_web/live/workout.ex
@@ -3,48 +3,17 @@ defmodule GymLiveWeb.GymLive.Workout do
 
   @impl true
   def mount(_params, _session, socket) do
-    if connected?(socket), do: :timer.send_interval(1000, self(), :tick)
-
-    socket
-    |> assign(seconds: 0)
-    |> then(&{:ok, &1})
+    socket = assign(socket, :workout_started, false)
+    {:ok, socket}
   end
 
   @impl true
-  def handle_info(:tick, socket) do
-    socket
-    |> assign(seconds: socket.assigns.seconds + 1)
-    |> then(&{:noreply, &1})
+  def handle_event("start_workout", _value, socket) do
+    {:noreply, assign(socket, :workout_started, true)}
   end
 
   def generate_workout_name do
     Timex.now()
     |> Timex.format!("{WDshort} {AM} Workout")
   end
-
-  def format_time(seconds) do
-    {h, m, s} = hhmmss(seconds)
-    "#{pad_int(h)}:#{pad_int(m)}:#{pad_int(s)}"
-  end
-
-  defp pad_int(int, padding \\ 2) do
-    Integer.to_string(int)
-    |> String.pad_leading(padding, "0")
-  end
-
-  @one_hour 3600
-  @one_minute 60
-
-  defp hhmmss(seconds), do: hhmmss(seconds, 0, 0, 0)
-
-  defp hhmmss(seconds, h, m, s) when seconds >= @one_hour,
-    do: hhmmss(seconds - @one_hour, h + 1, m, s)
-
-  defp hhmmss(seconds, h, m, s) when seconds >= @one_minute,
-    do: hhmmss(seconds - @one_minute, h, m + 1, s)
-
-  defp hhmmss(seconds, h, m, s) when seconds >= 1,
-    do: hhmmss(seconds - 1, h, m, s + 1)
-
-  defp hhmmss(_seconds, h, m, s), do: {h, m, s}
 end
