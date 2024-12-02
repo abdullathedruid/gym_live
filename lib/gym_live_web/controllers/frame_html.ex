27f6c39aa663013bda25fb2cf7b2cfca9262a692
abdullathedruid <abdulla.alkamil@gmail.com>
2024-12-02 19:19:19 +0000

    feat: change start_url

--- lib/gym_live_web/controllers/frame_html.ex
+++ lib/gym_live_web/controllers/frame_html.ex
@@ -2,8 +2,4 @@ defmodule GymLiveWeb.FrameHTML do
   use GymLiveWeb, :html
 
   embed_templates "frame_html/*"
-
-  def relative_path(path) do
-    "https://b291-2a02-6b6f-ea54-9900-895a-e62e-9f84-d485.ngrok-free.app#{path}"
-  end
 end
